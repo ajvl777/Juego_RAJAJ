ec183034dc8c92c0197408ff7c72d266870f6ac5
AntonioJ_Vallinot <a.j.vallinot@gmail.com>
2021-03-17 09:16:00 +0100

    Añadida la segunda parte argumental con sus metodos , la historia de la jefa .

--- src/juego_principal/juego_principal.java
+++ src/juego_principal/juego_principal.java
@@ -32,24 +32,70 @@ public class juego_principal {
 			}
 		} while (opcion < 1 || opcion > 3);
 		
+		if(opcion != 2) {
+			do {
+				System.out.println("\nEntonces llegas al trabajo, irónicamente trabajas en una pequeña oficina con 4 compañeros");
+				System.out.println("Al ser 21 de Julio , el calor es agobiante , y no hay aire acondicionado...");
+				System.out.println("De repente , entra tu jefa alterada ...Tiene algo que decir !");
+				System.out.println( "Elige una opcion y verás el resultado...");
+				System.out.println("OPCION 1\nOPCION 2\nOPCION 3");
+				opcion = scNum.nextInt();
+
+				switch (opcion) {
+					case 1:
+						noticiaJefa1();
+						break;
+					case 2:
+						noticiaJefa2();
+						break;
+					case 3:
+						noticiaJefa3();
+						break;
+					default:
+						System.out.println("Eleccion incorrecta.");
+					}
+			} while (opcion < 1 || opcion > 3);
+		}
+		
+		
 		
 		
 		System.out.println("FIN . Se acabó.");
 	}
 
-
-	
-
 	static void pisaMierda1() {
-		System.out.println("Disimulas , y mientras vas andando , arrastras la suela de la zapatilla disimuladamente...");
+		System.out
+				.println("Disimulas , y mientras vas andando , arrastras la suela de la zapatilla disimuladamente...");
 	}
 
 	static void pisaMierda2() {
-		System.out.println("Justo aparece la chica que te gusta , y mientras habla contigo , denota el edor ... y se aleja con una excusa y mirandote con cara rara...");
+		System.out.println(
+				"Justo aparece la chica que te gusta , y mientras habla contigo , denota el edor ... y se aleja con una excusa y mirandote con cara rara...");
+		System.out.println(
+				"A la desesperada , y debido a la distancia alzanado la voz , de tu boca sale lo siguiente : ");
+		System.out.println("No soy yo ...Yo me ducho !!! No soy raroooo....tia que me gustas mucho!!! ");
+		System.out.println("... ... ... ... obviamente .... esto es un GAME OVER !!!!");
 	}
 
 	static void pisaMierda3() {
-		System.out.println("Al ser tan supersticioso , continuas alegremente confiando en que hoy sera tu día de suerte .");
+		System.out.println(
+				"Al ser tan supersticioso , continuas alegremente confiando en que hoy sera tu día de suerte .");
+	}
+
+	static void noticiaJefa1() {
+		System.out.println("Tu jefa : ");
+		System.out.println("LO SABÍA!!! Es la tercera vez esta semana que la pisas !! Tus compañeros no pueden seguir así");
+		System.out.println("Estás despedido !! GAME OVER muchacho....Búscate un trabajo.");
+	}
+
+	static void noticiaJefa2() {
+		System.out.println("Chicos ! Día libre , nos vamos al Acuopoliiiiiiiiiis!!! wiiiiiiiiii");
+	}
+
+	static void noticiaJefa3() {
+		System.out.println("Atención! Teneis 2 horas para terminar el proyecto GitHub o estaréis en serios problemas .");
+		System.out.println("Podéis consultar a vuestro encargado , Rafa , que no bromeo ...no me tiembla el pulso a la hora de despedir a gente, y buscar nuevos empleados .");
+		System.out.println("El tiempo empieza ya !! Vamos.");
 	}
 
 }
